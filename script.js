97bc30a9bcb1532bb3789013fa189c21db32ba5f
antugacor <gacorantu63@gmail.com>
2025-03-07 11:49:23 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const orderButton = document.getElementById("orderButton");
+    orderButton.addEventListener("click", function() {
+        alert("Pesanan Anda telah diterima! Terima kasih telah memilih Roti Roi.");
+    });
+});
